740bb1f3cb9df0f8e534ac5c774072ce96cbc161
Norbert Niziołek <nizioleque@gmail.com>
2023-01-16 20:15:41 +0100

    Add project files.

---
+++ Sudoku/kernel.cu
@@ -0,0 +1,50 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include "utils.cuh"
+
+const char* boardFilename = "sudoku.txt";
+
+int main()
+{
+    cudaError_t cudaStatus;
+    // read text file
+    std::ifstream inFS;
+    inFS.open(boardFilename);
+
+    std::string line;
+    getline(inFS, line);
+    int nBoards = stoi(line);
+
+    char* originalBoards = new char[nBoards * 9 * 9];
+    int originalBoardsIndex = 0;
+
+    for (int board = 0; board < nBoards; board++) {
+        for (int row = 0; row < 9; row++) {
+            getline(inFS, line);
+            if (line.length() == 0) continue;
+            for (int column = 0; column < 9; column++) {
+                originalBoards[originalBoardsIndex++] = line[column] - '0';
+            }
+        }
+    }
+
+
+    // run CPU
+    for (int board = 0; board < nBoards; board++) {
+
+    }
+
+    //
+
+    // run GPU
+
+
+    return 0;
+}
+
---
+++ Sudoku/utils.cuh
@@ -0,0 +1,80 @@
+#include <iostream>
+
+void printBoard(char* board) {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			std::cout << +board[i * 9 + j];
+		}
+		std::cout << std::endl;
+	}
+}
+
+bool isCorrect(char* board) {
+	bool exists[10];
+
+	// check areas
+	for (int areaX = 0; areaX < 3; areaX++) {
+		for (int areaY = 0; areaY < 3; areaY++) {
+			std::fill_n(exists, 10, false);
+			int startX = 3 * areaX;
+			int startY = 3 * areaY;
+			for (int fieldX = 0; fieldX < 3; fieldX++) {
+				for (int fieldY = 0; fieldY < 3;fieldY++) {
+					int fieldIndex = (startY + fieldY) * 9 + (startX + fieldX);
+					int fieldValue = board[fieldIndex];
+					if (fieldValue == 0 || exists[fieldValue]) return false;
+					exists[fieldValue] = true;
+				}
+			}
+		}
+	}
+
+	// check rows
+	for (int row = 0; row < 9; row++) {
+		std::fill_n(exists, 10, false);
+		for (int fieldX = 0; fieldX < 9; fieldX++) {
+			int fieldValue = board[row * 9 + fieldX];
+			if (fieldValue == 0 || exists[fieldValue]) return false;
+			exists[fieldValue] = true;
+		}
+	}
+
+	// check columns
+	for (int column = 0; column < 9; column++) {
+		std::fill_n(exists, 10, false);
+		for (int fieldY = 0; fieldY < 9; fieldY++) {
+			int fieldValue = board[fieldY * 9 + column];
+			if (fieldValue == 0 || exists[fieldValue]) return false;
+			exists[fieldValue] = true;
+		}
+	}
+
+	return true;
+}
+
+bool isSafe(char* board, int x, int y, int value) {
+	// check area
+	int areaStartX = (x / 3) * 3;
+	int areaStartY = (y / 3) * 3;
+	for (int fieldX = 0; fieldX < 3; fieldX++) {
+		for (int fieldY = 0; fieldY < 3;fieldY++) {
+			int fieldIndex = (areaStartY + fieldY) * 9 + (areaStartX + fieldX);
+			int fieldValue = board[fieldIndex];
+			if (fieldValue == value) return false;
+		}
+	}
+
+	// check row
+	for (int fieldX = 0; fieldX < 9; fieldX++) {
+		int fieldValue = board[y * 9 + fieldX];
+		if (fieldValue == value) return false;
+	}
+
+	// check column
+	for (int fieldY = 0; fieldY < 9; fieldY++) {
+		int fieldValue = board[fieldY * 9 + x];
+		if (fieldValue == value) return false;
+	}
+
+	return true;
+}
